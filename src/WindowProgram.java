5f483833113e9694f252ba984530a07fa247a6c2
demerzel <mengzhaoxin@hotmail.com>
2018-10-19 00:17:52 +0200

    update

--- src/WindowProgram.java
+++ src/WindowProgram.java
@@ -33,44 +33,7 @@ public class WindowProgram implements ChatMessageListener, ActionListener {
             this.chat=chat;
             this.order=order;
         }
-        /**
-         * Compares this object with the specified object for order.  Returns a
-         * negative integer, zero, or a positive integer as this object is less
-         * than, equal to, or greater than the specified object.
-         *
-         * <p>The implementor must ensure <tt>sgn(x.compareTo(y)) ==
-         * -sgn(y.compareTo(x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
-         * implies that <tt>x.compareTo(y)</tt> must throw an exception iff
-         * <tt>y.compareTo(x)</tt> throws an exception.)
-         *
-         * <p>The implementor must also ensure that the relation is transitive:
-         * <tt>(x.compareTo(y)&gt;0 &amp;&amp; y.compareTo(z)&gt;0)</tt> implies
-         * <tt>x.compareTo(z)&gt;0</tt>.
-         *
-         * <p>Finally, the implementor must ensure that <tt>x.compareTo(y)==0</tt>
-         * implies that <tt>sgn(x.compareTo(z)) == sgn(y.compareTo(z))</tt>, for
-         * all <tt>z</tt>.
-         *
-         * <p>It is strongly recommended, but <i>not</i> strictly required that
-         * <tt>(x.compareTo(y)==0) == (x.equals(y))</tt>.  Generally speaking, any
-         * class that implements the <tt>Comparable</tt> interface and violates
-         * this condition should clearly indicate this fact.  The recommended
-         * language is "Note: this class has a natural ordering that is
-         * inconsistent with equals."
-         *
-         * <p>In the foregoing description, the notation
-         * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
-         * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
-         * <tt>0</tt>, or <tt>1</tt> according to whether the value of
-         * <i>expression</i> is negative, zero or positive.
-         *
-         * @param o the object to be compared.
-         * @return a negative integer, zero, or a positive integer as this object
-         * is less than, equal to, or greater than the specified object.
-         * @throws NullPointerException if the specified object is null
-         * @throws ClassCastException   if the specified object's type prevents it
-         *                              from being compared to this object.
-         */
+
         @Override
         public int compareTo(Node o) {
             if(o.order<order)
@@ -95,7 +58,7 @@ public class WindowProgram implements ChatMessageListener, ActionListener {
     }
 
     //0:total_sequencer 1:causal vector clock
-	int orderFlag=0;
+	int orderFlag=1;
 
 	JFrame frame;
 	JTextPane txtpnChat = new JTextPane();
@@ -217,10 +180,22 @@ public class WindowProgram implements ChatMessageListener, ActionListener {
         public void run(){
 	        while(true){
 	            if(!pbq.isEmpty()){
-
+                    Node node= null;
+                    try {
+                        node = pbq.take();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+//                    System.out.println("clientMessageCount :"+clientMessageCount+" "+"order: "+node.order);
+                    if(clientMessageCount==node.order){
+                        txtpnChat.setText(getTime()+" "+node.order+" "+node.name+" : "+node.chat + "\n" + txtpnChat.getText());
+                        clientMessageCount++;
+                    }else{
+                        pbq.add(node);
+                    }
                 }
                 try {
-                    sleep(500);
+                    sleep(2000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -246,6 +221,10 @@ public class WindowProgram implements ChatMessageListener, ActionListener {
 		responseChecker.start();
         LeaderChecker leaderChecker=new LeaderChecker();
         leaderChecker.start();
+        if(orderFlag==0){
+            PbqHolder pbqHolder=new PbqHolder();
+            pbqHolder.start();
+        }
  	}
 
 	private void initializeFrame() {
